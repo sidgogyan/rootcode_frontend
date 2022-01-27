ba199f346eb127462df72e107b332517b513d87e
siddharth <sid9057819561@gmail.com>
2022-01-28 00:16:15 +0530

    run_code_implemete

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Menu } from 'antd';
-
+import { Input, Space, Button, Switch ,Spin} from 'antd';
+import MyResult from './MyResult';
+import axios from 'axios'
 import brace from 'brace';
 import AceEditor  from 'react-ace';
 
@@ -9,14 +11,36 @@ import 'brace/mode/java';
 
 // Import a Theme (okadia, github, xcode etc)
 import 'brace/theme/monokai';
+import config from '../services/config';
+const baseURL=config.baseURL
 
 const { SubMenu } = Menu;
 
 
 
 const Editor = () => {
+  const [input, setInput] = React.useState("");
+  const [res, setResult] = React.useState(false);
+  const [code,setCode]=React.useState("")
+  const [output,setOutput]=React.useState("")
+  const [loading,setLoading]=React.useState(false)
+
+  const run_code=async()=>{
+
+    setLoading(true)
+    const myres= await axios.post(`${baseURL}/question/run_code`,{'code':code,'input':input,'langauge':'java'})
+    
+    // console.log(myres.data.output)
+    myres.data.output.stderr!=""? setOutput(myres.data.output.stderr):(myres.data.output.stdout!=""?setOutput(myres.data.output.stdout):setOutput(myres.data.output.errorType))
+    setInput("")
+    console.log(output)
+    setLoading(false)
+    
+  }
+
   return (
-   
+   <>
+    <Spin spinning={loading}>
     <div style={{border:'1.5px solid #e5e7eb'}} >
       <Menu mode="horizontal">
       <SubMenu key="SubMenu1" title="Theme">
@@ -51,15 +75,55 @@ const Editor = () => {
                     editorProps={{
                         $blockScrolling: true
                     }}
+                    onChange={(value)=>setCode(value)}
                     width='100%'
                     showPrintMargin={false}
-                    fontSize={20}
+                    fontSize={16}
                 />
+
+</div>
+<div></div>
+{input!=""?<span>  <h6 style={{marginTop:'20px'}}>Custom Input</h6>
+        <div style={{display:"flex"}} >
+       
+        <Input.TextArea rows={8}  style={{width:"40%"}}  className='card'  onChange={(e)=>setInput(e.target.value)} />
+        </div></span>:""}
+
+        {res && <MyResult/>}
+
+        {output!=""?<span>  <h6 style={{marginTop:'20px',marginLeft:"20px"}}>Output</h6>
+        <div style={{display:"flex"}} >
+       
+        <Input.TextArea rows={8}  style={{width:"40%",color:'#2d2d39',fontWeight:'500'}}  className='card' value={output} disabled   />
+        </div></span>:""}
+
+        
+       
+      
+        <div  class="btn btn--sub"  style={{marginLeft:"40px",marginTop:'10px'}} onClick={()=>run_code()}>Run</div>
+        <div  class="btn btn--sub"  onClick={() => {
+            setInput("");
+            setResult(true);
+          }} style={{marginLeft:"40px",marginTop:'10px'}}>Submit</div>
+        <span  style={{marginLeft:"52%"}}>Custom input</span>
+        <Switch
+        style={{marginLeft:"20px"}}
+          checked={input}
+          checkedChildren=""
+          unCheckedChildren=""
+          onChange={() => {
+            setInput(!input);
+            setResult(false);
+          }}
+          />
+
+    
 
      
     
+</Spin>
 
-</div>
+</>
   );
 };
 
--- src/Components/QuestionContent.js
+++ src/Components/QuestionContent.js
@@ -3,43 +3,18 @@ import Editor from './Editor';
 import ReactMarkdown from 'react-markdown'
 
 
-import { Input, Space, Button, Switch } from 'antd';
-import MyResult from './MyResult';
+
+
 
 
 const QuestionContent = ({content}) => {
-  const [input, setInput] = React.useState(false);
-  const [res, setResult] = React.useState(false);
+
   return (
       <>
        <ReactMarkdown>{content}</ReactMarkdown>
          <Editor/>
 
-        {input==true?<div>  <h6 style={{marginTop:'20px'}}>Custom Input</h6>
-        <div style={{display:"flex"}} >
-       
-        <Input.TextArea rows={8}  style={{width:"50%"}}  className='card' />
-        </div></div>:""}
-
-        {res && <MyResult/>}
-       
-      
-        <div  class="btn btn--sub"  style={{marginLeft:"40px",marginTop:'10px'}}>Run</div>
-        <div  class="btn btn--sub"  onClick={() => {
-            setInput(false);
-            setResult(true);
-          }} style={{marginLeft:"40px",marginTop:'10px'}}>Submit</div>
-        <span  style={{marginLeft:"52%"}}>Custom input</span>
-        <Switch
-        style={{marginLeft:"20px"}}
-          checked={input}
-          checkedChildren=""
-          unCheckedChildren=""
-          onChange={() => {
-            setInput(!input);
-            setResult(false);
-          }}
-          />
+        
        
         {/* </div> */}
          
